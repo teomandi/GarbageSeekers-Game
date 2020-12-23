f26dfbb86a9a9b9d5c8d8cc156c1b59b7d272504
teomandi <teomandi@gmail.com>
2020-12-23 16:05:16 +0200

    Game start, multi user control and freezer gun

---
+++ GarbageSeekers/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using HashTable = ExitGames.Client.Photon.Hashtable;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] GameObject cameraHolder;
+    [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+    [SerializeField] Item[] items;
+
+    int itemIndex;
+    int previousItemIndex = -1;
+
+    float verticalLookRotation;
+    bool grounded;
+    Vector3 smoothMoveVelocity;
+    Vector3 moveAmount;
+
+    Rigidbody rb;
+    PhotonView PV;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        PV = GetComponent<PhotonView>();
+    }
+    private void Start()
+    {
+        if (PV.IsMine)
+        {
+            EquipItem(0);
+        }
+        else
+        {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+            Destroy(rb);
+        }
+    }
+
+    private void Update()
+    {
+        if (!PV.IsMine)
+            return;
+        Look();
+        Move();
+        Jump();
+
+        //items handle from numbers
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+                EquipItem(i);
+                break;
+            }
+        }
+        //items handle from scrollwheel
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            if (itemIndex >= items.Length - 1)
+                EquipItem(0);
+            else
+                EquipItem(itemIndex + 1);
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            if (itemIndex <= 0)
+                EquipItem(items.Length - 1);
+            else
+                EquipItem(itemIndex - 1);
+        }
+    }
+    void Look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+    private void Move()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+    public void SetGroundedState(bool _grounded)
+    {
+        grounded = _grounded;
+        Debug.Log("gounded? : " + grounded);
+    }
+    private void FixedUpdate()
+    {
+        if (!PV.IsMine)
+            return;
+        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+
+    void EquipItem(int _index)
+    {
+        if (_index == previousItemIndex)
+            return;
+        itemIndex = _index;
+        items[itemIndex].itemGameObject.SetActive(true);
+
+        if (previousItemIndex != -1)
+        {
+            items[previousItemIndex].itemGameObject.SetActive(false);
+        }
+        previousItemIndex = itemIndex;
+
+        if (PV.IsMine)
+        {
+            HashTable hash = new HashTable();
+            hash.Add("itemIndex", itemIndex);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        }
+    }
+    //to share the weapon change among the other players
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, HashTable changedProps)
+    {
+        base.OnPlayerPropertiesUpdate(targetPlayer, changedProps);
+
+        if (!PV.IsMine && targetPlayer == PV.Owner)
+            EquipItem((int)changedProps["itemIndex"]);
+    }
+}
---
+++ GarbageSeekers/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO; //for Path
+
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+    private void Awake()
+    {
+        // classinc singleton script
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.buildIndex == 1) //we are in the level1 scene
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
+        }
+    }
+}
