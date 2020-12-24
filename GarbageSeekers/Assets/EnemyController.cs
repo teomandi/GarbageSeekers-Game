f67776c0783ae6bddfaed9ddbacfd3b07eb1010e
teomandi <teomandi@gmail.com>
2020-12-24 21:14:12 +0200

    Making an enemy moving to the player

---
+++ GarbageSeekers/Assets/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerTracker.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, transform.position);
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ GarbageSeekers/Assets/PlayerTracker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    #region
+    public static PlayerTracker instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public GameObject player;
+}
