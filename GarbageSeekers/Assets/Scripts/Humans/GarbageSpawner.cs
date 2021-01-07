c3fd801e72c068e98f7ca38f7175580db1098856
teomandi <teomandi@gmail.com>
2021-01-07 23:13:40 +0200

    Garbage spawning in random angle

---
+++ GarbageSeekers/Assets/Scripts/Humans/GarbageSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject garbagePrefab;
+    [SerializeField] float period=0.05f, possibility=1;
+    [SerializeField] int minThrowForce=400, maxThrowForce=800;
+
+
+    private void Start()
+    {
+        InvokeRepeating("ThrowGarbage", period, period);
+    }
+
+    void ThrowGarbage()
+    {
+        GameObject garbage = Instantiate(garbagePrefab) as GameObject;
+        garbage.transform.position = transform.position;
+        int throwForce = Random.Range(minThrowForce, maxThrowForce);
+        Rigidbody rb = garbage.GetComponent<Rigidbody>();
+
+        garbage.transform.rotation = Random.rotation;
+
+        Debug.Log(throwForce);
+
+        rb.AddForce(throwForce * new Vector3(1, 1, Random.Range(-1f, 1f)));
+    }
+
+
+}
